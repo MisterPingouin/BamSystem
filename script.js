ebac74f46e76559d1a680cf2d76dc23a35a969b8
Benjamin RICHARD <richard.benjamin69@yahoo.fr>
2023-03-15 16:17:43 +0100

    Update on css Top button added + js

--- script.js
+++ script.js
@@ -24,5 +24,22 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slides[slideIndex - 1].style.display = "block";
+  //slides[slideIndex - 1].style.display = "block";
+}
+//Button
+
+let mybutton = document.getElementById("myBtn");
+
+function scrollFunction() {
+  if (document.body.scrollTop > 1200 || document.documentElement.scrollTop > 1200) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+window.onscroll = function() {scrollFunction()};
+
+function topFunction() {
+  document.documentElement.scrollTop = 0;
 }
