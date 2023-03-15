6d2834f245490237dc4dcb81e894a5c2db90ffc3
maxdlr <contact@maxdlr.com>
2023-03-15 16:18:08 +0100

    Début de commencement de setup de gestion des couleurs des svg par le css pour facilité effets genre :hover

--- index.php
+++ index.php
@@ -2,12 +2,14 @@
 <html lang="en">
 <?php
 
+$pageTitle = 'Home';
+
 $link1 = '#planet-mars';
 $link2 = '#planet-moon';
 $link3 = '#planet-mercury';
 $link4 = '#planet-venus';
 
-    require 'html/shared/head.html';
+    require 'html/shared/head.php';
     include 'html/shared/nav.html';
     include 'html/shared/planet-nav.php';
     include 'html/content/index-content.html';
