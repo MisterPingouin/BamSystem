e39a80d94d2c59875a21321b3e8fc771e091c25f
MisterPingouin <anthony.axeldisplay@gmail.com>
2023-03-15 17:28:12 +0100

    On annule le rebase

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-// JS Page Rental //
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("custom-slider");
-
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  //slides[slideIndex - 1].style.display = "block";
-}
 //Button
 
 let mybutton = document.getElementById("myBtn");
