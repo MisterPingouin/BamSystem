a7411070e1d1418390396dd5b46a21bca5f00092
maxdlr <contact@maxdlr.com>
2023-03-15 12:15:27 +0100

    Mise en place de structure PHP et arrangements diverses des pages html.

---
+++ rental.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+
+    require 'html/shared/head.html';
+    include 'html/shared/nav.html';
+    include 'html/content/rental-content.html';
+    include 'html/shared/footer.html';
+    include 'html/shared/scripts.html';
+
+?>
+</html>
