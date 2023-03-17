edc2ebf5a0fb22db2c0fc9931331c4656fb374c6
maxdlr <contact@maxdlr.com>
2023-03-17 11:22:38 +0100

    Mise a jour de la page index: structure php avec variable, functions et automatisme du contenu des presentations de planetes

--- index.php
+++ index.php
@@ -10,11 +10,15 @@ $link3 = '#planet-mercury';
 $link4 = '#planet-venus';
 
     require 'html/shared/head.php';
+    echo "<body>";
     include 'html/shared/nav.html';
+    echo "<main>";
     include 'html/shared/planet-nav.php';
-    include 'html/content/index-content.html';
+    include 'html/content/index-content.php';
+    echo "</main>";
     include 'html/shared/footer.html';
     include 'html/shared/scripts.html';
+    echo "</body>";
 
 ?>
 </html>
