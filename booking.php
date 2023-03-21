452a308a025a6be5dc7a67dd7324212854ee75ac
MisterPingouin <anthony.axeldisplay@gmail.com>
2023-03-21 12:28:35 +0100

    small add

--- booking.php
+++ booking.php
@@ -10,7 +10,6 @@ $pageTitle = 'Booking';
     include 'html/shared/nav.html';
     include 'html/content/booking-content.html';
     include 'html/shared/footer.html';
-    include 'html/shared/scriptbooking.html';
     echo "</body>";
 ?>
 </html>
