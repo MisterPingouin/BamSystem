1dd066bef158393090b5d1f3012910aa8b2a920a
maxdlr <contact@maxdlr.com>
2023-03-21 13:57:59 +0100

    Structure php terminé pour la page rental

--- html/content/rental-content.php
+++ html/content/rental-content.php
@@ -1,10 +1,38 @@
 <?php
 
 $rentalMars = [
+    'titles' => [
+        'title 1',
+        'title 2',
+        'title 3'
+    ],
     'descriptions' => [
-        'Description 1',
-        'Description 2',
-        'Description 3'
+        'Description 1 Mars',
+        'Description 2 Mars',
+        'Description 3 Mars'
+    ],
+    'prices' => [
+        '468',
+        '562',
+        '789'
+    ],
+    'shipImgs' => [
+        'assets/ships/ship1.jpg',
+        'assets/ships/Ship2.jpg',
+        'assets/ships/Ship3.jpg'
+    ]
+];
+
+$rentalMoon = [
+    'titles' => [
+        'title 1',
+        'title 2',
+        'title 3'
+    ],
+    'descriptions' => [
+        'Description 1 Moon',
+        'Description 2 Moon',
+        'Description 3 Moon'
     ],
     'prices' => [
         '468',
@@ -19,10 +47,38 @@ $rentalMars = [
 ];
 
 $rentalMercury = [
+    'titles' => [
+        'title 1',
+        'title 2',
+        'title 3'
+    ],
     'descriptions' => [
-        'Description 1',
-        'Description 2',
-        'Description 3'
+        'Description 1 Mercury',
+        'Description 2 Mercury',
+        'Description 3 Mercury'
+    ],
+    'prices' => [
+        '468',
+        '562',
+        '789'
+    ],
+    'shipImgs' => [
+        'assets/ships/ship1.jpg',
+        'assets/ships/Ship2.jpg',
+        'assets/ships/Ship3.jpg'
+    ]
+];
+
+$rentalVenus = [
+    'titles' => [
+        'title 1',
+        'title 2',
+        'title 3'
+    ],
+    'descriptions' => [
+        'Description 1 Venus',
+        'Description 2 Venus',
+        'Description 3 Venus'
     ],
     'prices' => [
         '468',
@@ -37,38 +93,35 @@ $rentalMercury = [
 ];
 
 function makeRentalSection($rentalPlanetSelected) {
-    $packDescription1 = $rentalPlanetSelected['descriptions'][0];
-    $packDescription2 = $rentalPlanetSelected['descriptions'][1];
-    $packDescription3 = $rentalPlanetSelected['descriptions'][2];
 
-    $packPrice1 = $rentalPlanetSelected['prices'][0];
-    $packPrice2 = $rentalPlanetSelected['prices'][1];
-    $packPrice3 = $rentalPlanetSelected['prices'][2];
+    $rentalPackTitle1 = $rentalPlanetSelected['titles'][0];
+    $rentalPackTitle2 = $rentalPlanetSelected['titles'][1];
+    $rentalPackTitle3 = $rentalPlanetSelected['titles'][2];
+
+    $rentalPackDescription1 = $rentalPlanetSelected['descriptions'][0];
+    $rentalPackDescription2 = $rentalPlanetSelected['descriptions'][1];
+    $rentalPackDescription3 = $rentalPlanetSelected['descriptions'][2];
+
+    $rentalPackPrice1 = $rentalPlanetSelected['prices'][0];
+    $rentalPackPrice2 = $rentalPlanetSelected['prices'][1];
+    $rentalPackPrice3 = $rentalPlanetSelected['prices'][2];
 
     $rentalSlideImg1 = $rentalPlanetSelected['shipImgs'][0];
     $rentalSlideImg2 = $rentalPlanetSelected['shipImgs'][1];
     $rentalSlideImg3 = $rentalPlanetSelected['shipImgs'][2];
 
-    include "html/content/rental-pres-content.php";
+    include 'html/content/rental-slide.php';
+    echo "<div class='pack-choice'>";
+    include 'html/content/rental-pack.php';
+    echo "</div>";
 }
-
-
-
-// $pack1 = ['pack1', 'Pack 1', $packDescription1, $packPrice1];
-// $pack2 = ['pack2', 'Pack 2', $packDescription2, $packPrice2];
-// $pack3 = ['pack3', 'Pack 3', $packDescription3, $packPrice3];
-
-// $slide = [$packImg1, $packImg2, $packImg3];
-
-
-
 
 echo "<main class='main-rental'>";
 
 makeRentalSection($rentalMars);
+makeRentalSection($rentalMoon);
 makeRentalSection($rentalMercury);
-makeRentalSection($rentalMars);
-makeRentalSection($rentalMercury);
+makeRentalSection($rentalVenus);
 
 echo "</main>";
 
--- html/content/rental-pack.php
+++ html/content/rental-pack.php
@@ -1,12 +1,30 @@
 <?php
 
 echo ("
-<div class='$rentalPackClass'>
-    <h2>$rentalPackTitle</h2>
-    <p>$rentalPackDescription</p>
+<div class='pack pack1'>
+    <h2>$rentalPackTitle1</h2>
+    <p>$rentalPackDescription1</p>
     <button type='button' id='rentnow'>Rent it</button>
     <div class='packprice'>
-        <p>$rentalPackPrice</p>
+        <p>$rentalPackPrice1</p>
+        <img class='btclogo' src='assets/logo/bitcoin.png'>
+    </div>
+</div>
+<div class='pack pack2'>
+    <h2>$rentalPackTitle2</h2>
+    <p>$rentalPackDescription2</p>
+    <button type='button' id='rentnow'>Rent it</button>
+    <div class='packprice'>
+        <p>$rentalPackPrice2</p>
+        <img class='btclogo' src='assets/logo/bitcoin.png'>
+    </div>
+</div>
+<div class='pack pack3'>
+    <h2>$rentalPackTitle3</h2>
+    <p>$rentalPackDescription3</p>
+    <button type='button' id='rentnow'>Rent it</button>
+    <div class='packprice'>
+        <p>$rentalPackPrice3</p>
         <img class='btclogo' src='assets/logo/bitcoin.png'>
     </div>
 </div>
