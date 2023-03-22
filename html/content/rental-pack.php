25b3840e21f41a8f8f3bf9d9a303275fa88c6253
MisterPingouin <anthony.axeldisplay@gmail.com>
2023-03-22 20:41:28 +0100

    reback popup freshstart title page

--- html/content/rental-pack.php
+++ html/content/rental-pack.php
@@ -11,17 +11,17 @@ echo ("
       <div class='login-popup'>
         <div class='form-popup' id='popupForm'>
           <div class='form-container'>
+          <button type='button' class='btn cancel' onclick='closeForm()'>X</button>
             <h2>Please login</h2>
             <label for='email'>
               <strong>E-mail</strong>
             </label>
-            <input type='text' id='email' placeholder='Votre Email' name='email' required />
+            <input type='text' id='email' placeholder='Email' name='email' required />
             <label for='psw'>
               <strong>Password</strong>
             </label>
-            <input type='password' id='psw' placeholder='Votre Mot de passe' name='psw' required />
+            <input type='password' id='psw' placeholder='Password' name='psw' required />
             <button type='submit' class='btn'>Login</button>
-            <button type='button' class='btn cancel' onclick='closeForm()'>Closed</button>
           </div>
         </div>
       </div>
@@ -37,17 +37,17 @@ echo ("
       <div class='login-popup'>
         <div class='form-popup' id='popupForm'>
           <div class='form-container'>
+          <button type='button' class='btn cancel' onclick='closeForm()'>X</button>
             <h2>Please login</h2>
             <label for='email'>
               <strong>E-mail</strong>
             </label>
-            <input type='text' id='email' placeholder='Votre Email' name='email' required />
+            <input type='text' id='email' placeholder='Email' name='email' required />
             <label for='psw'>
               <strong>Password</strong>
             </label>
-            <input type='password' id='psw' placeholder='Votre Mot de passe' name='psw' required />
+            <input type='password' id='psw' placeholder='Password' name='psw' required />
             <button type='submit' class='btn'>Login</button>
-            <button type='button' class='btn cancel' onclick='closeForm()'>Closed</button>
           </div>
         </div>
       </div>
@@ -63,19 +63,18 @@ echo ("
       <div class='login-popup'>
         <div class='form-popup' id='popupForm'>
           <div class='form-container'>
+          <button type='button' class='btn cancel' onclick='closeForm()'>X</button>
             <h2>Please login</h2>
             <label for='email'>
               <strong>E-mail</strong>
             </label>
-            <input type='text' id='email' placeholder='Votre Email' name='email' required />
+            <input type='text' id='email' placeholder='Email' name='email' required />
             <label for='psw'>
               <strong>Password</strong>
             </label>
-            <input type='password' id='psw' placeholder='Votre Mot de passe' name='psw' required />
+            <input type='password' id='psw' placeholder='Password' name='psw' required />
             <button type='submit' class='btn'>Login</button>
-            <button type='button' class='btn cancel' onclick='closeForm()'>Closed</button>
           </div>
-
         </div>
       </div>
       <div class='packprice'>
