f7e473cd4207971f666111268f603591dc5210a0
Benjamin RICHARD <richard.benjamin69@yahoo.fr>
2023-03-22 15:26:46 +0100

    Titres ajoutés Hover supprimé Footer travel à fixer

--- travel.php
+++ travel.php
@@ -11,11 +11,11 @@ $link3 = "#mercury-pack";
 $link4 = "#venus-pack";
 
     require 'html/shared/head.php';
-    echo "<body>";
+    echo "<body class='darkerBg'>";
     include 'html/shared/nav.html';
     include 'html/shared/planet-nav.php';
     include 'html/content/travel-content.php';
-    include 'html/shared/footer.html';
+    include 'html/shared/footer-travel.html';
     include 'html/shared/scripts.html';
     echo "</body>";
 ?>
