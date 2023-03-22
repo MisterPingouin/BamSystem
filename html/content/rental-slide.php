1ed993c5b5ce35aeacb2882c33244eab119ec32b
maxdlr <contact@maxdlr.com>
2023-03-22 15:27:28 +0100

    refonte design de rental, reglage scrolling depuis planet nav sur les pages.

--- html/content/rental-slide.php
+++ html/content/rental-slide.php
@@ -3,29 +3,25 @@
 
 echo ("
 <div class='imagebackheadrental'>
-    <div>
-    <div class='slide-container'>
-        <div class='$customSlider fade'>
-          <div class='slide-index'>$planetName</div>
-          <img class='slide-img' src='$rentalSlideImg1'/>
-          <div class='slide-text'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </div>
-        </div>
+  
+  <div class='slide-container'>
     <div class='$customSlider fade'>
-        <div class='slide-index'>$planetName</div>
-        <img class='slide-img' src='$rentalSlideImg2' />
-        <div class='slide-text'>Nullam luctus aliquam ornare.</div>
-    </div>
-    <div class='$customSlider fade'>
-        <div class='slide-index'>$planetName</div>
-        <img class='slide-img' src='$rentalSlideImg3' />
-        <div class='slide-text'>
-          Praesent lobortis libero sed egestas suscipit.
-        </div>
-    </div>
-    <a class='prev' onclick='$plusBtn(-1)'>❮</a>
-    <a class='next' onclick='$plusBtn(1)'>❯</a>
+      <div class='slide-index'>$planetName</div>
+      <img class='slide-img' src='$rentalSlideImg1'/>
+      <div class='slide-text'></div>
+  </div>
+  <div class='$customSlider fade'>
+      <div class='slide-index'>$planetName</div>
+      <img class='slide-img' src='$rentalSlideImg2' />
+      <div class='slide-text'></div>
+  </div>
+  <div class='$customSlider fade'>
+      <div class='slide-index'>$planetName</div>
+      <img class='slide-img' src='$rentalSlideImg3' />
+      <div class='slide-text'></div>
+  </div>
+  <a class='prev' onclick='$plusBtn(-1)'>❮</a>
+  <a class='next' onclick='$plusBtn(1)'>❯</a>
 </div>
 
 ")
