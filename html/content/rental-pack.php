a7485b1ba80238e2dfb0d66b43a51908102b715b
MisterPingouin <[anthony.axeldisplay@gmail.com](mailto:anthony.axeldisplay@gmail.com)>
2023-03-23 15:16:03 +0100

    fix pop up, fix form

--- html/content/rental-pack.php
+++ html/content/rental-pack.php
@@ -7,7 +7,7 @@ echo ("
   <div id=rentalmars class='pack pack1'>
       <h2>$rentalPackTitle1</h2>
       <p>$rentalPackDescription1</p>
-      <button type='open-button' onclick='openForm()'>Rent it</button>
+      <button type='button' onclick='openForm()'>Rent it</button>
       <div class='login-popup'>
         <div class='form-popup' id='popupForm'>
           <div class='form-container'>
@@ -27,13 +27,13 @@ echo ("
       </div>
       <div class='packprice'>
           <p>$rentalPackPrice1</p>
-          <img class='btclogo' src='assets/logo/bitcoin.png'>
+          <img class='btclogo' src='assets/logo/bitcoin.png' alt='bitcoin'>
       </div>
   </div>
   <div id=rentalmoon class='pack pack2'>
       <h2>$rentalPackTitle2</h2>
       <p>$rentalPackDescription2</p>
-      <button type='open-button' onclick='openForm()'>Rent it</button>
+      <button type='button' onclick='openForm()'>Rent it</button>
       <div class='login-popup'>
         <div class='form-popup' id='popupForm'>
           <div class='form-container'>
@@ -53,13 +53,13 @@ echo ("
       </div>
       <div class='packprice'>
           <p>$rentalPackPrice2</p>
-          <img class='btclogo' src='assets/logo/bitcoin.png'>
+          <img class='btclogo' src='assets/logo/bitcoin.png' alt='bitcoin'>
       </div>
   </div>
   <div id=rentalmercury class='pack pack3'>
       <h2>$rentalPackTitle3</h2>
       <p>$rentalPackDescription3</p>
-      <button type='open-button' onclick='openForm()'>Rent it</button>
+      <button type='button' onclick='openForm()'>Rent it</button>
       <div class='login-popup'>
         <div class='form-popup' id='popupForm'>
           <div class='form-container'>
@@ -68,18 +68,18 @@ echo ("
             <label for='email'>
               <strong>E-mail</strong>
             </label>
-            <input type='text' id='email' placeholder='Email' name='email' required />
+            <input type='text' class='email' placeholder='Email' name='email' required />
             <label for='psw'>
               <strong>Password</strong>
             </label>
             <input type='password' id='psw' placeholder='Password' name='psw' required />
-            <button type='submit' class='btn'>Login</button>
+            <button type='submit' id='btn'>Login</button>
           </div>
         </div>
       </div>
       <div class='packprice'>
           <p>$rentalPackPrice3</p>
-          <img class='btclogo' src='assets/logo/bitcoin.png'>
+          <img class='btclogo' src='assets/logo/bitcoin.png' alt='bitcoin'>
       </div>
   </div>
 </section>
